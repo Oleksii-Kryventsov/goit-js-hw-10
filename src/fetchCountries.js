6560eccbf29b4e0b8d4a9b2de7fca819f064af89
Oleksii-Kryventsov <kryventsov@gmail.com>
2023-03-26 23:44:55 +0300

    some renews

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+export default function fetchCountries(nameOfCountry) {
+    return fetch (`${BASE_URL}/${nameOfCountry}?fields=name,capital,population,flags,languages`)
+        .then(response => {
+            if (response.ok) {
+            return response.json()
+        } throw new Error(response.status)
+    })
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './css/styles.css';
-
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import API from './fetchCountries.js'
 const DEBOUNCE_DELAY = 300;
+
+const searchCountryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+searchCountryInput.addEventListener('input', debounce(handleSearchCountry, DEBOUNCE_DELAY))
+
+function handleSearchCountry(event) {
+    event.preventDefault();
+    let searchCountry = event.target.value.trim();
+    clearInput();
+    if (searchCountry === "") {   
+        return;
+    }
+    API.fetchCountries(searchCountry).then(renderMarkup).catch(fetchError)
+};
+
+function renderMarkup(data) {
+    if (data.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (data.length >= 2 && data.length < 10) {
+        const createMarkup = data.map(element => {
+            const countryListMarkup = `
+            <li class="country-list__item">
+            <img class="country-list__image" src="${element.flags.png}" alt="${element.flags.alt}"></img>
+            <p class="country-list__text"><b>${element.name.official}</b></p>
+            </li>`;
+            return countryListMarkup
+        }).join('')
+        countryList.innerHTML = createMarkup;
+    }
+}
